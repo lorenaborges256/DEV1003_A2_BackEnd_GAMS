2ec48e262a5a706f6a1aacb1e5436621448175ef
lorenaborges256 <lorena.borges.amaral@gmail.com>
2026-05-29 15:18:35 +1000

    feat: create db.js to separate connection logic

---
+++ src/config/db.js
@@ -0,0 +1,14 @@
+// Connect to the database
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error('DB connection error:', err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
