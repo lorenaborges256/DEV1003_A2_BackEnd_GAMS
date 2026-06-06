c36a681e0ebdf0c5a7f765563621ad5e969022f2
Courtney Macgregor <cjoss90@hotmail.com>
2026-06-06 15:59:17 +1000

    refactor: move POST and DELETE watchlist routes to watchlistController

--- src/routes/watchlistRoutes.js
+++ src/routes/watchlistRoutes.js
@@ -5,47 +5,9 @@ const watchlistController = require('../controllers/watchlistController');
 
 const router = express.Router();
 
-router.post('/', verifyToken, async (request, response, next) => {
-  try {
-    const { targetId, targetType } = request.body;
+router.post('/', verifyToken, watchlistController.addToWatchlist);
 
-    const existing = await Watchlist.findOne({
-      user: request.user.id,
-      targetId,
-    });
-
-    if (existing) {
-      return response.status(409).json({ error: 'Already watching this item.' });
-    }
-
-    const watchlistEntry = await Watchlist.create({
-      user: request.user.id,
-      targetId,
-      targetType,
-    });
-
-    return response.status(201).json(watchlistEntry);
-  } catch (error) {
-    return next(error);
-  }
-});
-
-router.delete('/:id', verifyToken, async (request, response, next) => {
-  try {
-    const watchlistEntry = await Watchlist.findOneAndDelete({
-      _id: request.params.id,
-      user: request.user.id,
-    });
-
-    if (!watchlistEntry) {
-      return response.status(404).json({ error: 'Watchlist entry not found.' });
-    }
-
-    return response.status(200).json({ message: 'Removed from watchlist.' });
-  } catch (error) {
-    return next(error);
-  }
-});
+router.delete('/:id', verifyToken, watchlistController.removeFromWatchlist);
 
 router.get('/', verifyToken, watchlistController.getWatchlist);
 
